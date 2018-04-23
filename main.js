4f5cc3b8a3bc5cb57b64b29a2298e8a5263dbe07
AlexanderSRasmussen <32093781+AlexanderSRasmussen@users.noreply.github.com>
2018-04-23 11:47:09 +0200

    Did something

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var navbar = window.document.getElementById("myNavbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.scrollTop;
+console.log(sticky.scrollTop);
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+});
